a4aadd0c2e58afcc6a8d43c74f0b73461a8c8b3e
Shanna Smith <shannasmith@Mac.phub.net.cable.rogers.com>
2025-10-23 13:31:39 -0400

    rename file and implement solution

--- js/2025-10-23-two-sums.js
+++
@@ -1,5 +0,0 @@
-/**
- * 🧩 Kata: Two Sum
- * Given an array of numbers and a target, return the indices
- * of the two numbers that add up to the target.
- */
---
+++ js/twoSum.js
@@ -0,0 +1,22 @@
+/**
+ * 🧩 Kata: Two Sum
+ * Given an array of numbers and a sum, return the indices
+ * of the two numbers that add up to the sum.
+ */
+
+function twoSum(nums, sum) {
+  const map = new Map();
+  console.log('Input nums:', nums, 'sum:', sum)
+  for (let i = 0; i < nums.length; i++) {
+    const complement = sum - nums[i];
+    if (map.has(complement)) {
+      const answer = [map.get(complement), i]
+      console.log('Answer:', answer);
+      return answer;
+    }
+    map.set(nums[i], i);
+  }
+  return []; // return empty array if no solution found
+}
+
+twoSum([2, 7, 11, 15], 9); // answer: [0, 1]
