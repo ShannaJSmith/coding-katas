e1b6526413bdd2f3e709c232e8cdd40fd090f730
Shanna Smith <shannasmith@Mac.phub.net.cable.rogers.com>
2025-10-23 15:03:01 -0400

    add python kata question

---
+++ python/is_palindrome.py
@@ -0,0 +1,5 @@
+"""
+🧩 Kata: is_palindrome
+Write a function that checks if a given string is a palindrome. 
+A palindrome is a word, phrase, or sequence that reads the same forward and backward, ignoring spaces and capitalization.
+"""
