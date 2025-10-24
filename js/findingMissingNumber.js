fddc11109d84442f340d41dc714be59a1737b5b1
Shanna Smith <shannasmith@Mac.phub.net.cable.rogers.com>
2025-10-24 13:09:32 -0400

    implement findingMissingNumber solution

---
+++ js/findingMissingNumber.js
@@ -0,0 +1,27 @@
+/**
+ * 🧩 Kata: findingMissingNumber
+ * You’re given an array containing all numbers from 1 to n, except one number is missing. Find and return that missing number.
+ * Rules: The array is unsorted, there are no duplicates and if no number is missing, return 0.
+ * Examples: findMissingNumber([1, 2, 4, 5, 6]) ➜ 3
+ * findMissingNumber([2, 3, 1, 5]) ➜ 4
+ * findMissingNumber([1, 2, 3, 4, 5]) ➜ 0  // means nothing is missing
+ */
+
+function findMissingNumber(arr) {
+  arr.sort((a, b) => a - b); // sort array in ascending order
+  for (let i = 0; i < arr.length; i++) { // loop through the array to find break in sequence
+    // sequence should be i + 1 since array is 0 indexed but the array starts from 1
+    if (arr[i] !== i + 1) return i + 1; // return the missing number
+  }
+  return 0; // or if no gaps, so return 0
+}
+
+
+// Example usage:
+findMissingNumber([1, 2, 4, 5, 6]); // 3
+findMissingNumber([2, 3, 1, 5]); // 4
+findMissingNumber([1, 2, 3, 4, 5]); // 0
+
+// Comments: does not account for multiple missing numbers
+
+module.exports = { findingMissingNumber };
