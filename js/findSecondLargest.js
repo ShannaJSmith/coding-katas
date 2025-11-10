f0725b10e8c049a1676d417f2ddccf54a843ecfd
ShannaJSmith <shanna_smith987@hotmail.com>
2025-11-09 22:47:54 -0500

    solution for find second largest

---
+++ js/findSecondLargest.js
@@ -0,0 +1,29 @@
+/**
+ * 🧩 Kata: Find Second Largest
+ * Given an array of numbers, return the second largest distinct number.
+ * If there isn’t one (because all elements are equal or there’s only one), return null.
+ */
+
+function findSecondLargest(arr) {
+    if (arr.length < 2) return null;
+    
+    let largest = null;
+    let secondLargest = null;
+
+    for (num of arr) {
+        if(num === null || num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num !== largest && (secondLargest === null || num > secondLargest)) {
+            secondLargest = num;
+        }
+    }
+    console.log(secondLargest)
+        return secondLargest;
+}
+
+findSecondLargest([2, 1, 5, 8]) // 5
+findSecondLargest([6, 6, 6]) // null
+findSecondLargest([2, 6, 9, 2, 4]) // 6
+
+module.exports = { findSecondLargest };
