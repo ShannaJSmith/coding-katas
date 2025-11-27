b659c0bf950abaeb1e64d4c74f3629abfac5caa4
ShannaJSmith <shanna_smith987@hotmail.com>
2025-11-26 19:34:08 -0500

    solve longest distinct char

---
+++ js/longestTwoDistinct.js
@@ -0,0 +1,30 @@
+/**
+ * 🧩 Kata: longestTwoDistinct
+ * Given a string, return the length of the longest substring that contains at most 2 distinct characters.
+ * Substring = continuous part of the string.
+ */
+
+function longestTwoDistinct(s) {
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const set = new Set();
+
+    for (let j = i; j < s.length; j++) {
+      set.add(s[j]);
+
+      if (set.size > 2) break;
+
+      maxLength = Math.max(maxLength, j - i + 1);
+    }
+  }
+
+  return maxLength;
+}
+
+longestTwoDistinct("eceba")     // 3   "ece"
+longestTwoDistinct("ccaabbb")   // 5   "aabbb"
+longestTwoDistinct("a")         // 1
+longestTwoDistinct("abc")       // 2 ("ab" or "bc")
+
+module.exports = { longestTwoDistinct };
